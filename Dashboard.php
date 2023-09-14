0eb6ff455946622dade0059921401cf5af9101af
das2497 <115341956+das2497@users.noreply.github.com>
2023-09-14 20:20:25 +0530

    ddd

--- Dashboard.php
+++ Dashboard.php
@@ -9,19 +9,19 @@ html_header("Dashboard");
 $sql = "SELECT COUNT('pro_id') FROM `products`";
 $result = $conn->query($sql);
 $row = $result->fetch_assoc();
-$products_C= $row["COUNT('pro_id')"];
+$products_C = $row["COUNT('pro_id')"];
 
 $sql = "SELECT COUNT('cat_id') FROM `categories`";
 $result = $conn->query($sql);
 $row = $result->fetch_assoc();
-$categories_c= $row["COUNT('cat_id')"];
+$categories_c = $row["COUNT('cat_id')"];
 
-$sql = "SELECT COUNT('sales_id') FROM `sales` WHERE YEAR(`date`) = ".date("Y")." AND MONTH(`date`) =".date("m"). ";";
+$sql = "SELECT COUNT('sales_id') FROM `sales` WHERE YEAR(`date`) = " . date("Y") . " AND MONTH(`date`) =" . date("m") . ";";
 $result = $conn->query($sql);
 $row = $result->fetch_assoc();
-$sales= $row["COUNT('sales_id')"];
+$sales = $row["COUNT('sales_id')"];
 
-echo<<<EOT
+echo <<<EOT
 
   <main id="main" class="main">
 
@@ -84,122 +84,129 @@ echo<<<EOT
 			</div>
 		</div>
       </div>
+    </div> 
 
-    </section>
-
-    <br>
 	<div class="row">
-    <div class="col-lg-8">
-      <div class="card">
-        <div class="card-body">
-          <h5 class="card-title" style="text-align: center;">View Product Details</h5>
-          <br>
-		  <div style="overflow-x:auto;">
-		  <table class="table table-striped" style="text-align:center; white-space:nowrap;font-size: 15px; " id="dataTable" width="100%" cellspacing="0">
-            <thead>
-              <tr>
-                <th scope="col">Product Image</th>
-				<th scope="col">Product ID</th>
-                <th scope="col">Product Name</th>
-              	<th scope="col">Category name</th>
-              </tr>
-            </thead>
-            <tbody>
-EOT;
-$sql = "SELECT * FROM `products` LEFT JOIN `categories` ON products.categorie_id = categories.cat_id ORDER BY `products`.`pro_id` ASC LIMIT 5;";
-$result = $conn->query($sql);
-if ($result->num_rows > 0) {
-// output data of each row
-while($row = $result->fetch_assoc()) {
-
-	echo "<tr> <td>"."<img class='img-fluid' width='50' height='50' src='uploads/{$row["media_file"]}'></td>
-	<td>".$row["pro_id"]."</td>
-	<td>".$row["pro_name"]."</td>
-	<td>".$row["cat_name"]."</td></tr>";
-}
-
-echo"</tbody></table></div></div></div></div>";
-} else {
-echo "</tbody></table></div></div></div></div>";
-}
-
-echo<<<EOT
-
-<div class="col-lg-4">
-<div class="card">
-  <div class="card-body">
-	<h5 class="card-title" style="text-align: center;">View Sales Details</h5>
-	<br>
-	<div style="overflow-x:auto;">
-	<table class="table table-striped" style="text-align:center; white-space:nowrap;font-size: 15px; " id="dataTable" width="100%" cellspacing="0">
-	  <thead>
-		<tr>
-		  <th scope="col">Batch No</th>
-		  <th scope="col">Product Image</th>
-		  <th scope="col">Product Name</th>
-		  <th scope="col">Quentity</th>
-		  <th scope="col">Price</th>
-		</tr>
-	  </thead>
-	  <tbody>
-EOT;
-$sql = "SELECT * FROM `sales` LEFT JOIN stocks ON sales.stock_id = stocks.s_id LEFT JOIN products ON stocks.pro_id = products.pro_id ORDER BY `sales`.`sales_id` ASC LIMIT 5;";
-$result = $conn->query($sql);
-if ($result->num_rows > 0) {
-// output data of each row
-while($row = $result->fetch_assoc()) {
-
-echo "<tr> <td>".$row["batch_no"]."</td>
-<td>"."<img class='img-fluid' width='50' height='50' src='uploads/{$row["media_file"]}'></td>
-<td>".$row["pro_name"]."</td>
-<td>".$row["qty"]."</td>
-<td>".$row["price"]."</td></tr>";
-
-}
-
-echo"</tbody></table></div></div></div></div></div>";
-} else {
-echo "</tbody></table></div></div></div></div></div>";
-}
-
-$sql = "SELECT pro_name , batch_no, (SUM(quantity) - SUM(qty)) AS 'avlqty' FROM `sales` LEFT JOIN `stocks` ON sales.stock_id = stocks.s_id LEFT JOIN `products` ON `stocks`.`pro_id` = `products`.`pro_id` GROUP BY stocks.batch_no , stocks.pro_id;";
-$chart_arrayx='';
-$chart_arrayy='';
-$result = $conn->query($sql);
-if ($result->num_rows > 0) {
-    $chart_array='' ;
-    while($row = $result->fetch_assoc()) {
-		$chart_arrayx.= "'" . $row["pro_name"] ." #" .$row["batch_no"] ."',";
-		$chart_arrayy.=$row["avlqty"] . "," ;
-    }
-} 
-
-echo<<<EOT
-
-<div class="row">
-	<div class="col-xxl-12 col-md-4">
-
-		<div class="card" >
-
-		<h5 class="card-title" style="text-align: center;">Product Availability</h5>
-
-			<canvas id="mychart" style="padding: 1rem;"></canvas>
+	<div class="col-md-3">
+		<div class="card mt-0">
+			<div class="row">
+				<div class="col-md-12 border-left text-center pt-2">
+				<span class="text-muted">Average Monthly Sales</span>
+					<h3 class="mb-0 fw-bold">150</h3>
+				</div>
 			</div>
 		</div>
 	</div>
-<script>
-    new Chart("mychart", {
-        type: "bar",
-        data: {
-			labels: [$chart_arrayx],
-            datasets: [
-				{
-					data: [$chart_arrayy]
-                }
-			]
-        }
-    });
-</script>
+	<div class="col-md-3">
+		<div class="card mt-0 ">
+			<div class="row">
+				<div class="col-md-6">
+					<div class="peity_bar_bad left text-center mt-2">
+						<canvas width="50" height="24"></canvas>
+						</span>
+						<h6>+40%</h6>
+					</div>
+				</div>
+				<div class="col-md-6 border-left text-center pt-2">
+				<span class="text-muted">Average Monthly Profit</span>
+					<h3 class="mb-0 fw-bold">Rs. 4560</h3>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="col-md-3">
+		<div class="card mt-0">
+			<div class="row">
+				<div class="col-md-6">
+					<div class="peity_line_good left text-center mt-2">
+						<canvas width="50" height="24"></canvas>
+						</span>
+						<h6>+60%</h6>
+					</div>
+				</div>
+				<div class="col-md-6 border-left text-center pt-2">
+				<span class="text-muted">Total Profit</span>
+					<h3 class="mb-0 fw-bold">Rs. 5672</h3>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="col-md-3">
+		<div class="card mt-0">
+			<div class="row">
+				<div class="col-md-6">
+					<div class="peity_bar_good left text-center mt-2">
+					<canvas width="50" height="24"></canvas>
+						<h6>+30%</h6>
+					</div>
+				</div>
+				<div class="col-md-6 border-left text-center pt-2">
+				<span class="text-muted">Average Monthly Cost</span>
+					<h3 class="mb-0 fw-bold">Rs. 2560</h3>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="col-md-3">
+		<div class="card mt-0">
+			<div class="row">
+				<div class="col-md-6">
+					<div class="peity_bar_good left text-center mt-2">
+					<canvas width="50" height="24"></canvas>
+						<h6>+30%</h6>
+					</div>
+				</div>
+				<div class="col-md-6 border-left text-center pt-2">
+				<span class="text-muted">Total Cost</span>
+					<h3 class="mb-0 fw-bold">Rs. 2560</h3>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="col-md-6 offset-md-0">
+		<h1 class="text-center">Products Selling Chart</h1>
+		<canvas id="myPieChart"></canvas>		
+	</div>
+	<div class="col-md-3 offset-md-0">
+		<div class="card mt-0">
+			<div class="row">
+				<div class="col-md-6">
+					<div class="peity_bar_good left text-center mt-2">
+					<canvas width="50" height="24"></canvas>
+						<h6></h6>
+					</div>
+				</div>
+				<div class="col-md-6 border-left text-center pt-2">
+				<span class="text-muted">Best Selling Product</span>
+					<h3 class="mb-0 fw-bold">Cement</h3>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+    <script>
+        // Data for the pie chart
+        var data = {
+            labels: ['Cement', 'Metal', 'Bricks'],
+            datasets: [{
+                data: [30, 40, 30], // Specify your data values here
+                backgroundColor: ['#ff5d52', '#52ff74', '#52aeff'], 
+            }]
+        };
+
+        // Get the canvas element
+        var ctx = document.getElementById('myPieChart').getContext('2d');
+
+        // Create the pie chart
+        var myPieChart = new Chart(ctx, {
+            type: 'pie',
+            data: data,
+        });
+    </script>
+
+</section>
+
 
 EOT;
 
@@ -208,4 +215,3 @@ echo <<<EOT
 EOT;
 
 html_footer();
-?>
